7eab4056d4a14e16356312edc0876a57ed3df69d
Siyuan Su <siyuansu21@gmail.com>
2025-12-30 04:35:54 -0500

    Fixed file stuff

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
+import PIL
 import numpy as np
-import keras
-from keras import layers
-from tensorflow import data as tf_data
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from pathlib import Path
 
